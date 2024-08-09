0c64291268234ea1b94c67a1deca9fe78115d69a
Andrew Tamburino <andytamburino@gmail.com>
2024-08-08 22:53:24 -0400

    Updated Watchdog code to look at the propper dir.
    Added Doc files if stat
    
    Cleaned up some code

--- fileFolderAutomator.py
+++ fileFolderAutomator.py
@@ -1,12 +1,13 @@
 import logging
-import os
-import sys
-import time
+# import os
+# import sys
+# import time
 
 from os import scandir, rename
 from os.path import splitext, exists, join
 from shutil import move
 from time import sleep
+
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -14,12 +15,12 @@ from watchdog.events import FileSystemEventHandler
 source_dir = "/Users/Windows-hater/Downloads"
 
 #Destination Directory Folders
-dest_dir_image = "C:\Users\Windows-hater\OneDrive\Desktop\Images"
-dest_dir_documents = "C:\Users\Windows-hater\OneDrive\Desktop\Docs"
+dest_dir_image = "/Users/Windows-hater/Downloads/Images"
+dest_dir_documents = "/Users/Windows-hater/Downloads/Docs"
 
 # ? supported image types
 image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw",
-                    ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
+                    ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico", "avif"]
 # ? supported Document types
 document_extensions = [".doc", ".docx", ".odt",
                        ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
@@ -51,7 +52,7 @@ def move_file(dest, entry, name):
 class FileHandler(FileSystemEventHandler):
     # ? THIS FUNCTION WILL RUN WHENEVER THERE IS A CHANGE IN "source_dir"
     def on_modified(self, event):
-        with os.scandir(source_dir) as entries:
+        with scandir(source_dir) as entries:
             for entry in entries:
                 name = entry.name
                 self.check_image_files(entry, name) #Image files
@@ -63,21 +64,26 @@ class FileHandler(FileSystemEventHandler):
                 move_file(dest_dir_image, entry, name)
                 logging.info(f"Moved image file: {name}")
 
+    def check_document_files(self, entry, name):  # * Checks all Document Files
+        for documents_extension in document_extensions:
+            if name.endswith(documents_extension) or name.endswith(documents_extension.upper()):
+                move_file(dest_dir_documents, entry, name)
+                logging.info(f"Moved document file: {name}")
+
 
 # ! Watchdogs API Code (From Site)
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    logging.info(f'start watching directory {path!r}')
+    path = source_dir
     event_handler = FileHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
     try:
         while True:
-            time.sleep(1)
-    finally:
+            sleep(10)
+    except KeyboardInterrupt:
         observer.stop()
-        observer.join()
+    observer.join()
